77ef0e0066a586a0e5c888d13c7da7f40d38eb65
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2024-01-07 18:49:46 +0200

    Finished search bar initial styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,73 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
 
+function FilterInputs() {
+  function filterFunction(event) {
+    sessionStorage.setItem('FilterChoice', event.target.id);
+  }
+  return (
+    <section className={styles.FilterSection}>
+      <div className={styles.Filter}>
+        <p
+          className={styles.FilterOptions}
+          id="bestMatch"
+          onClick={filterFunction}
+        >
+          Best Match
+        </p>
+        <p
+          className={styles.FilterOptions}
+          id="highestRated"
+          onClick={filterFunction}
+        >
+          Highest Rated
+        </p>
+        <p
+          className={styles.FilterOptions}
+          id="mostReviewed"
+          onClick={filterFunction}
+        >
+          Most Reviewed
+        </p>
+      </div>
+      <hr />
+    </section>
+  );
+}
+
+function SearchInputs() {
+  return (
+    <div className={styles.Inputs}>
+      <input id="searchBusinessTerm" placeholder="Search Businesses" />
+      <input id="searchBusinessArea" placeholder="Where?" />
+    </div>
+  );
+}
+
+function SearchButton() {
+  function searchInfo() {
+    const searchBusinessTerm = document.getElementById('searchBusinessTerm');
+    const searchBusinessArea = document.getElementById('searchBusinessArea');
+
+    sessionStorage.setItem('location', searchBusinessArea.value);
+    sessionStorage.setItem('term', searchBusinessTerm.value);
+    sessionStorage.setItem('radius', 1000);
+    const sortBy = sessionStorage.getItem('FilterChoice');
+    sessionStorage.setItem('sortBy', sortBy);
+    console.log(sessionStorage);
+  }
+
+  return <button onClick={searchInfo}>Let's Go</button>;
+}
+
 function SearchBar() {
-  return <section className={styles.First}>SearchBar</section>;
+  return (
+    <section className={styles.SearchBar}>
+      <FilterInputs />
+      <SearchInputs />
+      <SearchButton />
+    </section>
+  );
 }
 
 export default SearchBar;

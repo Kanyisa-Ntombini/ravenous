6409bd08385d6d8f29be7cc0a8804632918611c4
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2024-01-07 18:54:24 +0200

    Finished initial styling

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Business from '../Business/Business';
 import styles from './BusinessList.module.css';
+import filterYelpData from '../../yelp_api/yelp_api';
 
 function BusinessList() {
-  const arr = [1, 2, 3];
+  const location = sessionStorage.getItem('location');
+  const term = sessionStorage.getItem('term');
+  const radius = sessionStorage.getItem('radius');
+  const sortBy = sessionStorage.getItem('sortBy');
+  const arrOfBusinesses = filterYelpData(location, term, radius, sortBy);
 
-  const businessList = arr.map((item, index) => {
-    return <Business key={index} />;
+  const businessList = arrOfBusinesses.map((business, index) => {
+    return (
+      <Business
+        key={index}
+        name={business.name}
+        imageSrc={business.imageSrc}
+        address={business.address}
+        city={business.city}
+        state={business.state}
+        zipCode={business.zipCode}
+        category={business.category}
+        rating={business.rating}
+        reviewCount={business.reviewCount}
+      />
+    );
   });
-  return <section className={styles.First}>{businessList}</section>;
+  return <section className={styles.BusinessList}>{businessList}</section>;
 }
 
 export default BusinessList;

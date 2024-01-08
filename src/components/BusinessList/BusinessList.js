fe08fc44f2a22f04fe87b1c8f49e4a01085aa583
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2024-01-08 14:03:05 +0200

    Removed errors

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,31 +2,6 @@ import React from 'react';
 import Business from '../Business/Business';
 import styles from './BusinessList.module.css';
 
-function BusinessListOld({ arrOfBusinesses }) {
-  if (arrOfBusinesses !== undefined) {
-    const businessList = arrOfBusinesses.map((business, index) => {
-      return (
-        <Business
-          key={index}
-          name={business.name}
-          imageSrc={business.imageSrc}
-          imageAlt={business.imageAlt}
-          address={business.address}
-          city={business.city}
-          state={business.state}
-          zipCode={business.zipCode}
-          category={business.category}
-          rating={business.rating}
-          reviewCount={business.reviewCount}
-        />
-      );
-    });
-    return <section className={styles.BusinessList}>{businessList}</section>;
-  } else {
-    return <section></section>;
-  }
-}
-
 function BusinessList(props) {
   const businessList = props.arrOfBusinesses.map((business, index) => {
     return (
